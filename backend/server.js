feaf2b43ca1b4649b5d0a5924b556c9e15395188
Tran Xuan Hoi <tranxuanhoi@Trans-MacBook-Pro.local>
2021-01-09 22:19:30 +0700

    add mongoose and fix api

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,30 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+    console.log('connect mongodb successful');
+});
 
+
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
-    res.send('Server is ready');
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find(x => x._id === req.params.id);
-    if(product){
-        return res.send(product)
-    }
-    res.status(404).send({message: 'Product not found'});
+  res.send('Server is ready');
 });
 
-app.get('/api/products/', (req, res) => {
-    res.send(data.products)
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
-})
+  console.log(`Serve at http://localhost:${port}`);
+});
